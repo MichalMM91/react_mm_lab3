8864b02bfd531e8018c40ba1b0726a3cdcf50cd7
MichalMM91 <48070450+MichalMM91@users.noreply.github.com>
2023-04-22 09:09:49 +0200

    add test comp with routing

--- src/components/LeftBar.js
+++ src/components/LeftBar.js
@@ -4,10 +4,12 @@ import React, {useState} from "react";
 import './style.css';
 import 'bulma/css/bulma.min.css';
 import { Icon } from '@iconify/react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Test from './Test';
 
 function LeftBar() {
     return (
-          
+          <Router>
         <div className="left-bar is-dark">
             <div className="block logo">
               <h1><Icon icon="mdi:cloud-circle-outline" /> SharpInterfaces.com</h1>
@@ -16,7 +18,7 @@ function LeftBar() {
             <aside className="menu">
               <ul className="menu-list">
                 <li><a className="is-active"><Icon icon="material-symbols:activity-zone" /> Dashboard</a></li>
-                <li><a><Icon icon="material-symbols:assistant-on-hub-sharp" /> Layouts</a></li>
+                <li><Link to="/Test"><Icon icon="material-symbols:assistant-on-hub-sharp" /> Layouts</Link></li>
               </ul>
               <p className=" m-2 menu-label">
                 Pages
@@ -44,6 +46,12 @@ function LeftBar() {
             </aside>
             
         </div>
+
+        <Routes>
+          <Route path="/Test" component={Test} />
+        </Routes>
+        </Router>
+
     )
     }
 
