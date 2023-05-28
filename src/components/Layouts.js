766d0c2a947e5b16415f447e350707d1f1a9777e
MichalMM91 <48070450+MichalMM91@users.noreply.github.com>
2023-05-28 21:36:36 +0200

    layouts

---
+++ src/components/Layouts.js
@@ -0,0 +1,15 @@
+
+import React, {useState} from "react";
+import 'bulma/css/bulma.min.css';
+import './style.css';
+
+function Layouts() {
+    return (
+        <div>
+            <h1>Got here using Router</h1>
+        </div>
+        
+    )
+    }
+
+export default Layouts;
